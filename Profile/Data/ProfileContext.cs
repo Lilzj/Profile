f3d51711795a7634bddc599f57a728983bf078f1
Gbogodor Joshua <joshuagbogodor@gmail.com>
2021-10-28 19:20:09 +0100

    created the project structure

---
+++ Profile/Data/ProfileContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Profile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Profile.Data
+{
+    public class ProfileContext : IdentityDbContext
+    {
+
+    }
+}
